5fdd1e13abeadbc9d1e10aa20c878bf2e9c325b0
Александр Бойко <boiko.erizoo@gmail.com>
2024-06-05 00:16:51 +0300

    - добавил файлы

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/erizoo/yandes-test-deploy',
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./{.env} ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
     },
   },
