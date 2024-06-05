953c8ee4ec71686a066b6ad6aa059f8c1f2fdcae
Александр Бойко <boiko.erizoo@gmail.com>
2024-06-05 10:30:39 +0300

    - тест

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: 'https://github.com/erizoo/yandes-test-deploy',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v21.7.3/bin/ && cd frontend && npm i && npm run build && pm2 startOrReload ecosystem.config.js',
+      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v16.20.2/bin/ && cd frontend && npm i && npm run build && pm2 startOrReload ecosystem.config.js',
     },
   },
 };
