f6983fe54f089139d13ff89edf2ace3a80ccc73e
Александр Бойко <boiko.erizoo@gmail.com>
2024-06-05 08:40:39 +0300

    - добавил файлы

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,7 +6,7 @@ const {
 
 module.exports = {
   apps: [{
-    name: 'api-service',
+    name: 'backend',
     script: './dist/app.js',
   }],
 
@@ -19,7 +19,7 @@ module.exports = {
       repo: 'https://github.com/erizoo/yandes-test-deploy',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v18.20.3/bin/ && cd backend && npm i && npm run build && pm2 restart all && pm2 startOrReload ecosystem.config.js',
+      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v18.20.3/bin/ && npm i && npm run build && pm2 restart all && pm2 startOrReload ecosystem.config.js',
     },
   },
 };
